9a9750b1764164060852c5d275e52bb4dc0192de
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-09-08 09:32:36 +0200

    create new component (recipe details)

--- SuperChef/src/app/app.module.ts
+++ SuperChef/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RecipeDetailsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
